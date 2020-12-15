d4ce6bc112faaed703d7c3d5303653ca532330d5
190093 <190093@e0505-07.JC-21.local>
2020-12-15 15:50:38 +0900

    課題1tst

--- src/Kadai01Test.java
+++ src/Kadai01Test.java
@@ -1,3 +1,4 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -5,10 +6,12 @@ import org.junit.jupiter.api.Test;
 class Kadai01Test {
 
 	@Test
-	void testReverseString() {
+	void testtestReverseString() {
 		Kadai01 k = new Kadai01();
-		assertNull(k.ReverseString(null));
-		assertEquals(4321,k.ReverseString("1234"));
-	}
-
+		assertNull(null,k.reverse(null));
+		assertEquals("4321",k.reverse("1234"));
+		assertEquals("",k.reverse(""));
+		assertEquals("おえういあ",k.reverse("あいうえお"));
+		assertEquals("321\n",k.reverse("\n123"));
+}
 }
