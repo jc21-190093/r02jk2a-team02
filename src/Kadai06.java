98007677d1ee09ade1d70ec8f80a313bfdf399f9
190655 <190655@e0505-06.JC-21.local>
2020-12-15 14:48:52 +0900

    課題６テストまで終わりました

--- src/Kadai06.java
+++ src/Kadai06.java
@@ -8,16 +8,21 @@ public class Kadai06 {
 	 * @return
 	 */
 	int get2ndMax(int[] d) {
+		if(d == null) {
+			return -1;
+
+		}
+		if(d.length==0) {
+			return -2;
+		}
 		int intMax2=0;
 		int intMax=d[0];
-
-		for(int i=1;i<d.length;i++){
-			if(intMax<d[i]) {
+		for(int i=0;i<d.length;i++){
+			if(intMax<=d[i]) {
 				intMax2=intMax;
 				intMax=d[i];
 			}
 		}
-
 		return intMax2;
 	}
 
